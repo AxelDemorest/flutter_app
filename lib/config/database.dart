3fafcf0f9bc2b06ef71f9764b0d7e4610ae99ef0
AxelDemorest <axel.demorest@gmail.com>
2022-11-23 00:12:27 +0100

    config: Connection to MongoDB database

---
+++ lib/config/database.dart
@@ -0,0 +1,13 @@
+import 'dart:developer';
+
+import 'package:mongo_dart/mongo_dart.dart';
+import 'package:flutter_app/constants.dart';
+
+class MongoDatabase {
+  static connect() async {
+    var db = await Db.create(mongoUrl);
+    await db.open();
+    inspect(db);
+    var collection = db.collection(collectionName);
+  }
+}
---
+++ lib/constants.dart
@@ -0,0 +1,2 @@
+const mongoUrl = "mongodb+srv://admin:admin@cluster0.3jun1xa.mongodb.net/flutter_project_database?retryWrites=true&w=majority";
+const collectionName = "users";
