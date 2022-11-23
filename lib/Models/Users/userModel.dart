a4b6a292726fc708aafa6c34c1a9b9b1a088e003
Paul Roumy <paul.roumy@edu.itescia.fr>
2022-11-23 10:32:49 +0100

    Creation class User et Rider

---
+++ lib/Models/Users/userModel.dart
@@ -0,0 +1,13 @@
+import 'package:mongo_dart/mongo_dart.dart';
+
+class User {
+  ObjectId id;
+  String name;
+  String lastName;
+  String email;
+  String phone;
+  int age;
+  bool isAdmin;
+
+  User(this.id, this.name, this.email, this.lastName, this.phone, this.age, this.isAdmin);
+}
