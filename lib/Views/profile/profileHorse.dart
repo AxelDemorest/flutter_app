c5b468dd2c6250ff06e781fc58d39eb1f07e1841
cmartin-vp <84844993+cmartin-vp@users.noreply.github.com>
2022-11-24 22:01:05 +0100

    Fixe add img horse

--- lib/Views/profile/profileHorse.dart
+++ lib/Views/profile/profileHorse.dart
@@ -1,9 +1,10 @@
 import 'dart:io';
-
+import 'dart:math' as math;
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:image_picker/image_picker.dart';
-import 'package:mongo_dart/mongo_dart.dart' as mongo;
+import 'package:path/path.dart';
+import 'package:path_provider/path_provider.dart';
 
 import '../../Models/horseModel.dart';
 
@@ -20,22 +21,36 @@ class _ProfileHorseState extends State<ProfileHorse> {
 
   File? _image;
   static const List<String> listSpe = <String>['Dressage', 'Saut d\'obstacle', 'Endurance', 'Complet'];
-  String dropdownValue = listSpe.first;
+  static const List<String> listSex = <String>['Male', 'Female'];
+  String dropdownValueSpe = listSpe.first;
+  String dropdownValueSex = listSex.first;
   var name = TextEditingController();
   var age = TextEditingController();
   var robe = TextEditingController();
   var race = TextEditingController();
   var sex = TextEditingController();
+
+  late Horse horse;
+
   @override
   Widget build(BuildContext context) {
+    horse =  ModalRoute.of(context)!.settings.arguments as Horse;
 
-    Future getImage() async {
+    Future<File> saveImgHorse(String imagePath) async {
+      final directory = await getApplicationDocumentsDirectory();
+      final name = '${basename(imagePath)}${(math.Random().nextDouble())}';
+      final image =  File('$directory/$name');
+
+      return File(imagePath).copy(image.path);
+    }
+
+    Future getImage(ImageSource source) async {
       try{
-        final image = await ImagePicker().pickImage(source: ImageSource.gallery);
+        final image = await ImagePicker().pickImage(source: source);
 
         if (image == null) return;
 
-        final imgTemp = File(image.path);
+        final imgTemp = await saveImgHorse(image.path);
 
         setState(() {
           _image = imgTemp;
@@ -46,15 +61,12 @@ class _ProfileHorseState extends State<ProfileHorse> {
       }
     }
 
-    Horse horse = Horse(mongo.ObjectId(), 'Le nom du cheval 111', 12, 'robe',
-          'race', 'sex', '1', 'assets/imgHorse/cheval1.jpeg');
     setState(
       () {
         name.text = horse.name;
         age.text = '${horse.age}';
         robe.text = horse.robe;
         race.text = horse.race;
-        sex.text = horse.sex;
       },
     );
 
@@ -79,8 +91,9 @@ class _ProfileHorseState extends State<ProfileHorse> {
                           padding: const EdgeInsets.all(20),
                           child: ClipRRect(
                             borderRadius: BorderRadius.circular(15),
-                            child: Image(
-                              image: AssetImage(horse.image),
+                            child: _image != null ? Image.file(_image!, width: 200, fit: BoxFit.contain,) :
+                            const Image(
+                              image: AssetImage('assets/imgHorse/cheval1.jpeg'),
                               width: 200,
                               fit: BoxFit.contain,
                             ),
@@ -142,20 +155,8 @@ class _ProfileHorseState extends State<ProfileHorse> {
                                   hintText: 'La race',
                                 )
                             ),
-                            TextFormField(
-                                controller: sex,
-                                textAlign: TextAlign.center,
-                                decoration: const InputDecoration(
-                                  enabledBorder: UnderlineInputBorder(
-                                      borderSide: BorderSide(
-                                          color: Colors.orange
-                                      )
-                                  ),
-                                  hintText: 'Le sexe',
-                                )
-                            ),
                             DropdownButton<String>(
-                              value: dropdownValue,
+                              value: dropdownValueSex,
                               icon: const Icon(Icons.arrow_downward),
                               elevation: 16,
                               underline: Container(
@@ -163,7 +164,26 @@ class _ProfileHorseState extends State<ProfileHorse> {
                               ),
                               onChanged: (String? value) {
                                 setState(() {
-                                  dropdownValue = value!;
+                                  dropdownValueSex = value!;
+                                });
+                              },
+                              items: listSex.map<DropdownMenuItem<String>>((String value) {
+                                return DropdownMenuItem<String>(
+                                  value: value,
+                                  child: Text(value),
+                                );
+                              }).toList(),
+                            ),
+                            DropdownButton<String>(
+                              value: dropdownValueSpe,
+                              icon: const Icon(Icons.arrow_downward),
+                              elevation: 16,
+                              underline: Container(
+                                color: Colors.orange,
+                              ),
+                              onChanged: (String? value) {
+                                setState(() {
+                                  dropdownValueSpe = value!;
                                 });
                               },
                               items: listSpe.map<DropdownMenuItem<String>>((String value) {
@@ -175,7 +195,7 @@ class _ProfileHorseState extends State<ProfileHorse> {
                             ),
                             ElevatedButton(
                                 onPressed: (){
-                                  getImage();
+                                  getImage(ImageSource.gallery);
                                 },
                                 child:
                                     Row(
